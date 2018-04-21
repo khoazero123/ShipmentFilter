e797a965c69c28bd1ffb85d42858d14a7a8d368f
Võ Văn Khoa <khoazero123@gmail.com>
2018-04-21 10:27:07 +0700

    Remove check Price > 0

--- app/code/local/Fgc/ShipmentFilter/Model/Observer.php
+++ app/code/local/Fgc/ShipmentFilter/Model/Observer.php
@@ -9,21 +9,20 @@ class Fgc_ShipmentFilter_Model_Observer {
 		//$quote = $observer->getQuote();
 
 		$quote = Mage::getModel("checkout/session")->getQuote();
-		$address = $quote->getShippingAddress();
-
 		$amount = $quote->getShippingAddress()->getShippingAmount();
-		if($amount > 0) {
-			$newPrice = $amount + ($amount * 10 / 100);
-			$store = Mage::app()->getStore($quote->getStoreId());
-			$carriers = Mage::getStoreConfig('carriers', $store);
-			foreach ($carriers as $carrierCode => $carrierConfig) {
-				//if ($carrierCode == 'premiumrate') {
-					$store->setConfig("carriers/{$carrierCode}/handling_type", 'P'); // F - Fixed, P - Percentage
-					$store->setConfig("carriers/{$carrierCode}/handling_fee", 10);
-					// ##If you want to set the price instead of handling fee you can simply use as:
-					//$store->setConfig("carriers/{$carrierCode}/price", $newPrice);
-				//}
-			}
+
+		$newPrice = $amount + ($amount * 10 / 100);
+		$store = Mage::app()->getStore($quote->getStoreId());
+		$carriers = Mage::getStoreConfig('carriers', $store);
+		foreach ($carriers as $carrierCode => $carrierConfig) {
+			//if ($carrierCode == 'premiumrate') {
+				$store->setConfig("carriers/{$carrierCode}/handling_type", 'P'); // F - Fixed, P - Percentage
+				$store->setConfig("carriers/{$carrierCode}/handling_fee", 10);
+				// ##If you want to set the price instead of handling fee you can simply use as:
+				// $store->setConfig("carriers/{$carrierCode}/price", $newPrice);
+			//}
 		}
+
+		$_shippingRateGroups = $address->getEstimateRates();
 	}
 }
